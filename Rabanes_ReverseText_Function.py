e68b6112f5f98f60bdda5124140ba5a0bdd340d6
sicnarFderflA <144906631+sicnarFderflA@users.noreply.github.com>
2024-01-03 19:47:59 +0800

    Update Rabanes_ReverseText_Function.py
    replaces spaces as empty string

--- Rabanes_ReverseText_Function.py
+++ Rabanes_ReverseText_Function.py
@@ -2,7 +2,7 @@ def reverse():
     while True:
         text = input("Enter a string: ")
         
-        if text.isalpha(): #checks if the variable text is a string.
+        if text.replace(' ', '').isalpha(): #checks if the variable text is a string. replaces space as empty string.
             rtext = '' #eversed Text.
             for char in text:
                 rtext = char + rtext
